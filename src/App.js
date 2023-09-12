c9d919af8b1df6d40c7433c5d5bd6efa0bc77049
shedrackejike <shedrackejike3@gmail.com>
2023-09-12 15:12:11 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import {
-  ChakraProvider,
-
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import Nav from './Components/nav';
 import Footer from './Components/Footer';
 import Mainsection from './pages/firstpage/Mainsection';
 import Customer from './pages/SecondPage/Customer';
 import WhyOak from './pages/Thirdpages/WhyOak';
 
-import {BrowserRouter, Route, Routes,} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-<Nav/>
-<Routes>
-   <Route path ="/" index element={<Mainsection/>}/>
-   <Route path="/customer" element={<Customer/>}/>
-   <Route path='/WhyOak' element={<WhyOak/>}/>
- </Routes> 
-<Footer/>
- </BrowserRouter>
-     </ChakraProvider>
+        <Nav />
+        <Routes>
+          <Route path="/" index element={<Mainsection />} />
+          <Route path="/Customer" element={<Customer />} />
+          <Route path="/WhyOak" element={<WhyOak />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
